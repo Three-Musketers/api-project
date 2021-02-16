924b63d926f6db9408f9096caf5e1cf2db33a8f2
hkmichae <heathermichaels@icloud.com>
2021-02-16 12:04:47 -0500

    Add files/folders HM

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// import NavBar from './components/NavBar'
+import { Switch, Route } from 'react-router-dom'
+// import Home from './components/Home'
+
+function App(props) {
+    return (
+        <div className="App">
+            <NavBar />
+            {/* <Switch>
+                <Route exact path="/" render={(props) => <Home {...props} />} />
+                <Route exact path="/RandomBeer" render={(props) => <RandomBeer {...props} />} />
+                <Route exact path="/NewBeer" render={(props) => <NewBeer {...props} />} />
+                <Route exact path="/AllBeers" render={(props) => <AllBeers {...props} />} />
+                <Route exact path="/allBeers/:id" render={(props) => <BeerDetail {...props} />} />
+                <Route exact path="/iron-rest" render={(props) => <IronRest {...props} />} />
+            </Switch> */}
+        </div>
+    );
+}
+
+export default App;
