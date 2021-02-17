9193d9b0ad1866d400fd10ea80448a23da8400cb
Victor Nwani <itsvictornwani@yahoo.com>
2021-02-17 16:15:29 -0500

    victor the committer

--- mood-board-app/src/components/Advice.js
+++ mood-board-app/src/components/Advice.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// axios.get('https://api.adviceslip.com/advice')
+//     .then(res => console.log(res))
 
 function Advice(props) {
+
+    const [advice, setAdvice] = useState("")
+
+    useEffect(() => {
+        axios.get('https://api.adviceslip.com/advice')
+            .then(res => {
+                console.log(res)
+                setAdvice(res.data.slip.advice)
+            })
+
+        // let res = await axios.get('https://api.adviceslip.com/advice')
+        // console.log(res)
+
+    }, [])
+
+
     return (
         <div>
-
+            {advice}
         </div>
     );
 }
