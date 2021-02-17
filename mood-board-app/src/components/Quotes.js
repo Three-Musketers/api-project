661946755eaeb4163042f7aacdf90fff499aeb7e
Victor Nwani <itsvictornwani@yahoo.com>
2021-02-17 17:15:22 -0500

    victor the committer

--- mood-board-app/src/components/Quotes.js
+++ mood-board-app/src/components/Quotes.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import "./Quotes.css"
+
 
 function Quotes(props) {
+
+    let [quotes, setQuotes] = useState([])
+
+    useEffect(() => {
+        axios.get('https://type.fit/api/quotes')
+            .then(res => {
+                console.log(res)
+                setQuotes(res.data.slice(0, 250))
+            })
+    })
     return (
-        <div>
-            Quotes
+        <div className="quotesContainer">
+            {quotes?.map(eachQuote => {
+                return (
+                    <p>{eachQuote.text}</p>
+                )
+            })}
         </div>
     );
 }
