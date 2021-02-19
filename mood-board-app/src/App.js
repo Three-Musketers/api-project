1f3f9546e2b8beb82a21c72ce992676be3e43a4d
hkmichae <heathermichaels@icloud.com>
2021-02-19 11:35:45 -0500

    Home Page HM

--- mood-board-app/src/App.js
+++ mood-board-app/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './index.css';
 import NavBar from './components/NavBar';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Quotes from './components/Quotes';
 import Advice from './components/Advice';
 import Jokes from './components/Jokes';
+import MoodHappy from './components/MoodHappy';
+import MoodSad from './components/MoodSad';
+import MoodAngry from './components/MoodAngry';
+import MoodScared from './components/MoodScared';
 import axios from 'axios'
 
 function App(props) {
@@ -19,7 +24,10 @@ function App(props) {
         <Route exact path="/Quotes" render={(props) => <Quotes {...props} />} />
         <Route exact path="/Advice" render={(props) => <Advice {...props} />} />
         <Route exact path="/Jokes" render={(props) => <Jokes {...props} />} />
-
+        <Route exact path="/MoodHappy" render={(props) => <MoodHappy {...props} />} />
+        <Route exact path="/MoodSad" render={(props) => <MoodSad {...props} />} />
+        <Route exact path="/MoodAngry" render={(props) => <MoodAngry {...props} />} />
+        <Route exact path="/MoodScared" render={(props) => <MoodScared {...props} />} />
       </Switch>
     </div>
   );
--- mood-board-app/src/components/Home.js
+++ mood-board-app/src/components/Home.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import backgroundImg from './assets/backgrounds/main-background.png';
+import happyB from './assets/buttons/button-happy.png';
+import sadB from './assets/buttons/button-sad.png';
+import angryB from './assets/buttons/button-angry.png';
+import scaredB from './assets/buttons/button-scared.png';
+import MoodHappy from './MoodHappy';
+import MoodSad from './MoodSad';
+import MoodAngry from './MoodAngry';
+import MoodScared from './MoodScared';
 
 function Home(props) {
     return (
-        <div>
+        <div className="Home-page">
+            <div id="Mood">
+                <img src={backgroundImg} alt="What is your mood?" width="1276px" />
+            </div>
 
+            <div className="Buttons" id="Box">
+                <Link to="/MoodHappy"><img src={happyB} alt="Happy" width="275px" /></Link>
+                <Link to="/MoodSad"><img src={sadB} alt="Sad" width="275px" /></Link>
+                <Link to="/MoodAngry"><img src={angryB} alt="Angry" width="275px" /></Link>
+                <Link to="/MoodScared"><img src={scaredB} alt="Scared" width="275px" /></Link>
+            </div>
         </div>
     );
 }
